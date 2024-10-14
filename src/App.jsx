ff7c956ceedaa47fe9a807e6d8a7db874001df0f
BERNARDOBOJALIL <bernardo.bojalil.lorenzini@gmail.com>
2024-10-14 17:47:22 -0600

    Project Base

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {ToDoList} from './components/ToDoList'
+import {FormTask} from './components/FormTask'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> ToDoList ✔ </h1>
+      <FormTask />
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
